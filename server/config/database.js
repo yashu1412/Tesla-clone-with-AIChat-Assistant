4c8223b36667dd8f3a02c9d4169ba6229c8f3d3e
yashu1412 <135001156+yashu1412@users.noreply.github.com>
2025-05-23 11:50:30 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,29 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+// });
+
+// pool.connect()
+//   .then(() => console.log('✅ PostgreSQL connected'))
+//   .catch((err) => {
+//     console.error('❌ PostgreSQL connection error:', err);
+//     process.exit(1);
+//   });
+
+// module.exports = pool;
+
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  connectionString: process.env.DATABASE_URL
 });
 
 pool.connect()
